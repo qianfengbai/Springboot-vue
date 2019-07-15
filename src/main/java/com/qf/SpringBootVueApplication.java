bed2a3382f195bb87458e833b73ad9c99e7ffe9a
baizhenjia <1368249116@qq.com>
2019-07-15 16:23:00 +0800

    第七组项目

---
+++ src/main/java/com/qf/SpringBootVueApplication.java
@@ -0,0 +1,16 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 54110 on 2019-07-15.
+ */
+@SpringBootApplication
+public class SpringBootVueApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootVueApplication.class,args);
+    }
+}
